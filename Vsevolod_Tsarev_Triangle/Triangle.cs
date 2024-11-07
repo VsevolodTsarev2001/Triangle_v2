53a5f9edd6e8b196a45b72979b5e16233f74b610
VsevolodTsarev2001 <153904999+VsevolodTsarev2001@users.noreply.github.com>
2024-11-07 10:01:15 +0200

    Add files via upload

---
+++ Vsevolod_Tsarev_Triangle/Triangle.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tringle
+{
+    public class Triangle
+    {
+        public double A;
+        public double B;
+        public double C;
+        public double S;//pindala
+        public double H;//kõrgus
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public Triangle(double s, double a)
+        {
+            S = s;
+            A = a;
+        }
+        public Triangle() { }
+        public bool ExistTriangle
+        {
+            get
+            {
+                if (A + B > C && A + C > B && B + C > A)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public double GetSetA
+        {
+            get { return A; }
+            set { A = value; }
+        }
+        public double GetSetB
+        {
+            get { return B; }
+            set { B = value; }
+        }
+        public double GetSetC
+        {
+            get { return C; }
+            set { C = value; }
+        }
+        public double GetSetS
+        {
+            get
+            { return S; }
+            set
+            { S = value; }
+        }
+        public string TriangleType
+        {
+            get
+            {
+                if (ExistTriangle)
+                {
+                    if (A == B && B == C && A == C)
+                    {
+                        return "võrdkülgne";
+                    }
+                    else if (A == B || A == C || B == C)
+                    {
+                        return "võrdhaarne";
+                    }
+                    else
+                    {
+                        return "erikülgne";
+                    }
+                }
+                else
+                {
+                    return "tundmatu tüüp";
+                }
+
+            }
+        }
+        public double Perimeter()
+        {
+            return A + B + C;
+        }
+        public double Perimeter1_2()
+        {
+            return (A + B + C) / 2;
+        }
+        public double Area()
+        {
+            double s = 0;
+            if (ExistTriangle)
+            {
+                double p;
+                p = (A + B + C) / 2;
+                s = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            }
+            return s;
+        }
+        public string OutputA()
+        {
+            return A.ToString();
+        }
+        public string OutputB()
+        {
+            return B.ToString();
+        }
+        public string OutputC()
+        {
+            return C.ToString();
+        }
+    }
+}
---
+++ Vsevolod_Tsarev_Triangle/Tringle_Vorm2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tringle
+{
+    public partial class Tringle_Vorm2 : Form
+    {
+        private double sideA;   // Сторона A
+        private double height;  // Высота
+        private Panel drawingPanel;  // Панель для рисования треугольника
+
+        public Tringle_Vorm2()
+        {
+            SetupUI();
+        }
+
+        private void SetupUI()
+        {
+            this.Text = "Minu kolmnurg";
+            this.Size = new Size(500, 400);
+            this.BackColor = Color.LightGreen;
+
+            Label labelA = new Label
+            {
+                Text = "Külg A:",
+                Location = new Point(10, 20),
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                ForeColor = Color.Red
+            };
+            Controls.Add(labelA);
+
+            TextBox textBoxA = new TextBox
+            {
+                Name = "textBoxA",
+                Location = new Point(150, 20),
+                Size = new Size(100, 30)
+            };
+            Controls.Add(textBoxA);
+
+            Label labelHeight = new Label
+            {
+                Text = "Kõrgus:",
+                Location = new Point(10, 60),
+                Font = new Font("Arial", 12, FontStyle.Bold),
+                ForeColor = Color.Red
+            };
+            Controls.Add(labelHeight);
+
+            TextBox textBoxHeight = new TextBox
+            {
+                Name = "textBoxHeight",
+                Location = new Point(150, 60),
+                Size = new Size(100, 30)
+            };
+            Controls.Add(textBoxHeight);
+
+            Button calculateButton = new Button
+            {
+                Text = "Arvuta",
+                Location = new Point(150, 100),
+                Size = new Size(100, 40),
+                BackColor = Color.LightYellow,
+                Font = new Font("Arial", 12, FontStyle.Regular)
+            };
+            calculateButton.Click += CalculateButton_Click;
+            Controls.Add(calculateButton);
+
+            // Панель для рисования треугольника
+            drawingPanel = new Panel
+            {
+                Size = new Size(300, 300),
+                Location = new Point(200, 150),
+                BackColor = Color.White
+            };
+            Controls.Add(drawingPanel);
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(Controls["textBoxA"].Text, out sideA) &&
+                double.TryParse(Controls["textBoxHeight"].Text, out height))
+            {
+                // Перерисовываем панель для отображения треугольника
+                this.Invalidate();
+                drawingPanel.Invalidate();
+            }
+            else
+            {
+                MessageBox.Show("Palun sisestage kehtivad väärtused.");
+            }
+        }
+
+        // Метод рисования треугольника
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+
+        // Рисуем треугольник в пределах панели
+        private void DrawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            // Если данные корректны (сторона и высота)
+            if (sideA > 0 && height > 0)
+            {
+                // Вычисление координат треугольника
+                int panelWidth = drawingPanel.ClientSize.Width;
+                int panelHeight = drawingPanel.ClientSize.Height;
+
+                // Масштабирование для оптимального размещения треугольника в панели
+                double scale = Math.Min(panelWidth / sideA, panelHeight / height) * 0.8;
+
+                // Центрирование треугольника на панели
+                int centerX = panelWidth / 2;
+                int centerY = panelHeight / 2;
+
+                // Точки треугольника
+                Point[] points = new Point[3];
+                points[0] = new Point(centerX, centerY - (int)(height * scale)); // Верхняя точка
+                points[1] = new Point(centerX - (int)(sideA * scale / 2), centerY + (int)(height * scale / 2)); // Левая нижняя точка
+                points[2] = new Point(centerX + (int)(sideA * scale / 2), centerY + (int)(height * scale / 2)); // Правая нижняя точка
+
+                // Рисуем треугольник
+                using (Pen pen = new Pen(Color.Blue, 3))
+                {
+                    e.Graphics.DrawPolygon(pen, points);
+                }
+            }
+        }
+    }
+}
